f4f2a1fc6f156d84c4531fd44bd984c76ff4726f
SarahRHollingworth <79085249+SarahRHollingworth@users.noreply.github.com>
2023-03-23 14:05:50 +0000

    Animations for Player Direction & basic Attacks fixed
    Basic attack needs cooldown implemented on it, but will do that next.

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,40 +7,87 @@ public class PlayerAttack : MonoBehaviour
     // Attributes
     public GameObject player;
     public GameObject manager;
+    public GameObject attack;
+    private SpriteRenderer sr;
 
     private PlayerBehaviour playerBehaviour;
     private AnimationManager am;
 
     private Animator animator;
 
+    bool attacking;
+
     // Start is called before the first frame update
     void Start()
     {
         playerBehaviour = player.GetComponent<PlayerBehaviour>();
+        
         am = manager.GetComponent<AnimationManager>();
-
         animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Attack says direction is " + playerBehaviour.GetDirection());
-      
+
 
         //This should be a switch statement for Case "Right", "Up", "Down", but it just exists as is for proof of concept. Fix this later.
 
 
-        if (playerBehaviour.GetDirection() == "Right")
-        {
-        animator.runtimeAnimatorController = am.GetAttackAnimatorController(1);
+       
 
-        }
-        else
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            animator.runtimeAnimatorController = am.GetAttackAnimatorController(0);
+            sr.enabled = true;
+            attacking = true;
+            animator.enabled = true;
+
+
+            Debug.Log("Attackkkkk!!!");
+
+
+            switch (playerBehaviour.GetDirection())
+            {
+                case "Right":
+                    sr.flipX = false;
+                    animator.runtimeAnimatorController = am.GetAttackAnimatorController(1);
+                    break;
+                case "Left":
+                    sr.flipX = true;
+                    animator.runtimeAnimatorController = am.GetAttackAnimatorController(1);
+                    break;
+                case "Up":
+                    animator.runtimeAnimatorController = am.GetAttackAnimatorController(0);
+                    break;
+                case "Down":
+                    animator.runtimeAnimatorController = am.GetAttackAnimatorController(2);
+                    break;
+            }
+    
+           
+    
+
+
+        }else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.enabled = false;
+            attacking = false;
+            sr.enabled = false;
         }
 
-        
+
+
     }
-}
+
+    public bool IsAttacking()
+    {
+        return attacking;
+
+    }
+
+
+    }
+
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,9 +9,11 @@ public class PlayerBehaviour : MonoBehaviour
     // Attributes
     private float speed;
     public GameObject manager;
+    public GameObject attack;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private AnimationManager am;
+    private PlayerAttack atk;
 
     private Animator animator;
 
@@ -25,7 +27,9 @@ public class PlayerBehaviour : MonoBehaviour
         speed = 4f;
         rb = GetComponent<Rigidbody2D>(); //Rigid Body for Player
         sr = GetComponent<SpriteRenderer>();    //Sprite Renderer for Player. Use later.
+        atk = attack.GetComponent<PlayerAttack>();
         am = manager.GetComponent<AnimationManager>();  //Animation Manager Class.
+
 
         sr.sprite = spriteArray[0]; //Needs updating for idle sprites.
 
@@ -61,14 +65,21 @@ public class PlayerBehaviour : MonoBehaviour
         return new Vector2(inputHorizontal, inputVertical);
     }
 
-    Vector2 CalcVelocity(Vector2 direction) 
-    { 
+     Vector2 CalcVelocity(Vector2 direction) 
+    {
+
+        if (atk.IsAttacking())
+        {
+            return new Vector2((direction.x * speed) / 2, (direction.y * speed) / 2);
+        }
         return new Vector2(direction.x * speed, direction.y * speed);   //Moves sprite in accordance to player input in GetDirecitonalInput();
+
     }
 
     void SetPlayerOrientation(Vector2 direction, Vector2 velocity)
     {
         // if we are receving vertical directional input
+
 
 
         if (direction.y != 0)
@@ -79,16 +90,16 @@ public class PlayerBehaviour : MonoBehaviour
                 animator.runtimeAnimatorController = am.GetWalkingAnimatorController(1);        //Access AnimationController in AnimationManager script to know which animation in the list to play according to index. 
 
                 CharacterIsFacingDirection = "Up";  //Updates player orientation as a string for comparison in player Attack function. If CharacterIsFacingDirection = "Up" then the attack animation for Up will be accessed, for example.
-                    Debug.Log(CharacterIsFacingDirection); //For testing, console should output current direction.
-         
+                Debug.Log(CharacterIsFacingDirection); //For testing, console should output current direction.
+
             }
             else
             {
                 animator.runtimeAnimatorController = am.GetWalkingAnimatorController(2);
 
-                    CharacterIsFacingDirection = "Down";
-                    Debug.Log(CharacterIsFacingDirection);
-                
+                CharacterIsFacingDirection = "Down";
+                Debug.Log(CharacterIsFacingDirection);
+
 
             }
         }
@@ -96,7 +107,7 @@ public class PlayerBehaviour : MonoBehaviour
         {
 
             animator.runtimeAnimatorController = am.GetWalkingAnimatorController(0);
-            sr.sprite = spriteArray[0];
+            // sr.sprite = spriteArray[0];
 
 
             if (direction.x < 0)
@@ -106,21 +117,21 @@ public class PlayerBehaviour : MonoBehaviour
             }
             else
             {
-               CharacterIsFacingDirection = "Right";
+                CharacterIsFacingDirection = "Right";
             }
-            
+
             Debug.Log(CharacterIsFacingDirection);
 
 
         }
-        
+
         //Flips Horizontal Sprite Left or Right depending on user input for A or D.
         if (direction.x != 0)
         {
             //sr.flipX = velocity.x < 0;
             if (velocity.x < 0)
             {
-                 sr.flipX = true;
+                sr.flipX = true;
 
             }
             else
@@ -128,7 +139,43 @@ public class PlayerBehaviour : MonoBehaviour
                 sr.flipX = false;
             }
         }
+
+
+        if (direction.y == 0 && direction.x == 0)
+        {
+
+            PlayerIdlingOrientation();
+        }
+
        
+    }
+
+    void PlayerIdlingOrientation()
+    {
+
+        switch (CharacterIsFacingDirection)
+        {
+            case "Up":
+                animator.runtimeAnimatorController = null;
+                sr.sprite = spriteArray[1];
+                break;
+            case "Down":
+                animator.runtimeAnimatorController = null;
+                sr.sprite = spriteArray[2];
+                break;
+            case "Right":
+            case "Left":
+                animator.runtimeAnimatorController = null;
+                sr.sprite = spriteArray[0];
+                break;
+            default:
+                Debug.Log("This condition should never be met, but it's here for bug testing");
+                sr.sprite = spriteArray[0];
+                break;
+        }
+    
+
+      
     }
 
 
